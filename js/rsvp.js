1b951eaab4d99604be228d385408364b06427aff
d4rkr3v3nge <103220531+d4rkr3v3nge@users.noreply.github.com>
2026-09-07 19:21:11 -0400

    Update RSVP redirect URL with form data

--- js/rsvp.js
+++ js/rsvp.js
@@ -26,8 +26,10 @@ function submitRSVP(event) {
         body: JSON.stringify(formData)
     })
     .then(() => {
-        window.location.href = "https://d4rkr3v3nge.github.io/ShaneAndSteph1/thankyou.html";
-    })
+window.location.href =
+  "https://d4rkr3v3nge.github.io/ShaneAndSteph1/thankyou.html?" +
+  new URLSearchParams(formData).toString();
+
     .catch(err => {
         alert("Error submitting RSVP.");
         console.error(err);
