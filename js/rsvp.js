97b428627e74b256ffc975488626719ac29f87de
d4rkr3v3nge <103220531+d4rkr3v3nge@users.noreply.github.com>
2026-09-07 17:29:07 -0400

    Update fetch URL in rsvp.js

--- js/rsvp.js
+++ js/rsvp.js
@@ -17,7 +17,7 @@ function submitRSVP(event) {
         userAgent: navigator.userAgent
     };
 
-    fetch("https://script.google.com/macros/s/AKfycbxux4PsgxtC91wV9uoaT3OmdHkYOpYvm6UFCRqZkLlw-jm8Zn3f2kf7fDPhEdNUcX9k/exec", {
+    fetch("https://script.google.com/macros/s/AKfycby8dOb2t-QcGidGaNOcrPDVEGWx1SXLeDZAJZ3ImVeEZWzubKJUBwRYVOAS7aMEBuFx/exec", {
         method: "POST",
         mode: "no-cors",
         headers: {
