ec56a4743eb7ca7f38cbbb75480d3536fe41a5de
d4rkr3v3nge <103220531+d4rkr3v3nge@users.noreply.github.com>
2026-09-07 16:55:59 -0400

    Refactor RSVP submission to use formData object

--- js/rsvp.js
+++ js/rsvp.js
@@ -1,38 +1,32 @@
-/**
- * @see https://developers.google.com/apps-script/guides/services/authorization
- */
-
-
-
 function submitRSVP(event) {
     event.preventDefault();
 
     const form = document.getElementById("rsvpForm");
-    const data = new FormData(form);
-
-    data.append("token", "8Y5Wv99i8PW%b0");
-    data.append("ip", "");
-    data.append("ua", navigator.userAgent);
+    const formData = {
+        name: form.name.value,
+        email: form.email.value,
+        attending: form.attending.value,
+        street: form.street.value,
+        city: form.city.value,
+        state: form.state.value,
+        zipcode: form.zipcode.value,
+        guests: form.guests.value,
+        message: form.message.value,
+        token: "8Y5Wv99i8PW%b0",
+        ip: "",
+        userAgent: navigator.userAgent
+    };
 
     fetch("https://script.google.com/macros/s/AKfycbwGcPJ86yEdCEFBBMPXiXGugA4jgeYqm_w7NotQAr80_YrDBQ3B7Hr1KbpjcHdlPoo/exec", {
-    method: "POST",
-    mode: "no-cors",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(formData)
-});
-
-    .then(res => res.text())
-    .then(result => {
-        if (result === "OK") {
-            // Redirect to thank-you page
-            window.location.href = "https://d4rkr3v3nge.github.io/ShaneAndSteph1/thankyou.html";
-        } else if (result === "RATE_LIMIT") {
-            alert("Too many submissions. Please try again later.");
-        } else {
-            alert("Submission blocked.");
-        }
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(() => {
+        window.location.href = "https://d4rkr3v3nge.github.io/ShaneAndSteph1/thankyou.html";
     })
     .catch(err => {
         alert("Error submitting RSVP.");
